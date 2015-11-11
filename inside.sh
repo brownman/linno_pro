1d7dbea4b41e94a91ea1a8dd8611e1db8f71a720
brownman <brownman556@gmail.com>
2015-11-11 16:52:48 +0200

    Update inside.sh

--- inside.sh
+++ inside.sh
@@ -4,7 +4,7 @@ subject=${2:-college}
 run_type=${3:-all}
 step_focus=${4:-}
 
-git clone git@gitlab.linnovate.net:brownman/dockerizing_projects.git /tmp/dockerizing_projects
+git clone https://gitlab.linnovate.net/brownman/dockerizing_projects.git /tmp/dockerizing_projects
 cd /tmp/dockerizing_projects
 chmod +x .ci.sh
 ./.ci.sh $user $subject $run_type $step_focus
