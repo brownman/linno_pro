1d182599339405da3db7ecd50a51710033a5483f
brownman <brownman556@gmail.com>
2015-11-11 18:47:14 +0200

    Update inside.sh

--- inside.sh
+++ inside.sh
@@ -3,8 +3,10 @@ user=${1:-user}
 subject=${2:-college}
 run_type=${3:-all}
 step_focus=${4:-}
+
+
 test -d  /tmp/dockerizing_projects || { \
-git clone https://gitlab.linnovate.net/brownman/dockerizing_projects.git /tmp/dockerizing_projects
+(git clone https://gitlab.linnovate.net/brownman/dockerizing_projects.git /tmp/dockerizing_projects) || (git clone git@gitlab.linnovate.net:brownman/dockerizing_projects.git /tmp/dockerizing_projects)
 }
 
 cd /tmp/dockerizing_projects
