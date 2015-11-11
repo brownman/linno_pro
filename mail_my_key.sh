4b9e6de6ddceeead0508738449cd91cad5c296fe
brownman <brownman556@gmail.com>
2015-11-11 14:34:46 +0200

    Update mail_my_key.sh
    [skip ci]

--- mail_my_key.sh
+++ mail_my_key.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 dev_user=shahar
+domain=linnovate
+endings=net
 sudo apt-get install mailutils -y -q
 sudo apt-get install sendmail -y -q
-cat $HOME/.ssh/id_rsa.pub  | mail -s "from $LOGNAME my_pub_key:" $dev_user@linnovate.net
+cat $HOME/.ssh/id_rsa.pub  | mail -s "from $LOGNAME my_pub_key:" ${dev_user}@${domain}.${endings}
