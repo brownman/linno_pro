652943a53bd67f4e2e481098de6f77c94f6d4170
brownman <brownman556@gmail.com>
2015-11-19 14:16:50 +0200

    Update report_dev.sh

--- report_dev.sh
+++ report_dev.sh
@@ -6,11 +6,12 @@ source config.cfg
 file_report="/tmp/all_$(date +%s)"
 trap_exit1(){
     local res=$?
-
   print func
-  commander_try "sleep 10; ./mail_for_fix.sh brownman '$LOGNAME] $res' $file_report"
+  subject="$LOGNAME]  $( date +%H:%M:%S) ] $res"
+
+  commander_try "sleep 10; ./mail_for_fix.sh brownman '$subject' $file_report"
 }
 cmd_hold_fingers="./outside.sh run ./inside.sh"
 export -f trap_exit1
-trap 'trap_exit1' EXIT
+#trap 'trap_exit1' EXIT
 ( trap 'trap_exit1' EXIT; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
