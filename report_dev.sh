49dd408021f1e55c842d88695634d26536cde930
brownman <brownman556@gmail.com>
2015-11-19 13:38:14 +0200

    Update report_dev.sh

--- report_dev.sh
+++ report_dev.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set +e
+set -u
+cd /tmp/linno_pro
 file_report="/tmp/all_$(date +%s)"
 
-cmd_hold_fingers="./outside.sh run ./inside.sh &>$file_report"
+cmd_hold_fingers="./outside.sh run ./inside.sh"
 
-( commander_try "$cmd_hold_fingers" )  || ( commander ./mail_for_fix.sh brownman \'$cmd_hold_fingers\' $file_report )
+commander_try "$cmd_hold_fingers" &> >(tee $file_report)  || ( commander_try ./mail_for_fix.sh brownman \'$cmd_hold_fingers\' $file_report )
