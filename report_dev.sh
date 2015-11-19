808a2726986b764208bade5298f774e0794aeb29
brownman <brownman556@gmail.com>
2015-11-19 13:44:57 +0200

    Update report_dev.sh

--- report_dev.sh
+++ report_dev.sh
@@ -6,4 +6,4 @@ file_report="/tmp/all_$(date +%s)"
 
 cmd_hold_fingers="./outside.sh run ./inside.sh"
 
-commander_try "$cmd_hold_fingers" &> >(tee $file_report)  || ( commander_try ./mail_for_fix.sh brownman \'$cmd_hold_fingers\' $file_report )
+( commander_try "$cmd_hold_fingers" &> >(tee $file_report) )  || ( commander_try "sleep 10; ./mail_for_fix.sh brownman \'$cmd_hold_fingers\' $file_report" )
