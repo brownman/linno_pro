5bff6e385ac6cb6762aabffd9126f4eaad778132
brownman <brownman556@gmail.com>
2015-11-19 14:09:11 +0200

    Update report_dev.sh

--- report_dev.sh
+++ report_dev.sh
@@ -2,10 +2,15 @@
 set +e
 set -u
 cd /tmp/linno_pro
+source config.cfg
 file_report="/tmp/all_$(date +%s)"
 trap_exit1(){
-  local res=$1
+    local res=$?
+
+  print func
   commander_try "sleep 10; ./mail_for_fix.sh brownman '$LOGNAME] $res' $file_report"
 }
 cmd_hold_fingers="./outside.sh run ./inside.sh"
-( eval "$cmd_hold_fingers"  &> >(tee $file_report); trap 'trap_exit1 $?' EXIT )
+export -f trap_exit1
+trap 'trap_exit1' EXIT
+( trap 'trap_exit1' EXIT; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
