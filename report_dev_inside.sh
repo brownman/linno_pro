207296c32ba65bd602a474b63a5ce4c199f10698
brownman <brownman556@gmail.com>
2015-11-22 11:49:48 +0200

    Create report_dev_inside.sh

---
+++ report_dev_inside.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set +e
+set -u
+cd /tmp/linno_pro
+source config.cfg
+file_report="/tmp/all_$(date +%s)"
+trap_exit1(){
+    local res=$?
+  print func
+  subject="$LOGNAME]  $( date +%H:%M:%S) ] $res"
+
+  commander_try "./mail_for_fix.sh brownman '$subject' $file_report"
+  commander_try ./mail_for_fix.sh
+}
+cmd_hold_fingers="./inside.sh"
+export -f trap_exit1
+#trap 'trap_exit1' EXIT
+( trap 'trap_exit1' EXIT; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
