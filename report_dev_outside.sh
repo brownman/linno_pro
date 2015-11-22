8957175c52a2425d4d8d768da27f766a891c4db4
brownman <brownman556@gmail.com>
2015-11-22 12:17:31 +0200

    Update report_dev_outside.sh

--- report_dev_outside.sh
+++ report_dev_outside.sh
@@ -4,7 +4,7 @@ set -u
 cd /tmp/linno_pro
 source config.cfg
 file_report="/tmp/all_$(date +%s)"
-trap_exit1(){
+trap_exit_outside(){
     local res=$?
   print func
   subject="$LOGNAME]  $( date +%H:%M:%S) ] $res"
@@ -13,6 +13,6 @@ trap_exit1(){
   commander_try ./mail_for_fix.sh
 }
 cmd_hold_fingers="./outside.sh run ./report_dev_inside.sh"
-export -f trap_exit1
+export -f trap_exit_outside
 #trap 'trap_exit1' EXIT
-( trap 'trap_exit1' EXIT; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
+( trap 'trap_exit_outside' EXIT; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
