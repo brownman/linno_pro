ae64c6d3b3b4b0688d5116bb467e3e2afd79ea6b
ci <ci@gmail.com>
2015-11-22 14:22:44 +0200

    fix: docker run - fix path

--- outside.sh
+++ outside.sh
@@ -10,7 +10,7 @@ set_env(){
     container_id='brownman/linno_pro:master'
     alias_ubuntu=alias_ubuntu1
     volume_apparmor='-v /usr/lib/x86_64-linux-gnu/libapparmor.so.1.1.0:/usr/lib/x86_64-linux-gnu/libapparmor.so.1:ro'
-    volume_ssh="-v $HOME/.ssh:/$HOME_INSIDE/.ssh"
+    volume_ssh="-v $HOME/.ssh:$HOME_INSIDE/.ssh"
     
     volume_tmp="-v /tmp/linno_pro_tmp:/tmp"
 
