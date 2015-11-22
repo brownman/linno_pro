7257a705b42f425cc2e608cb16c3a04ef4739f2f
brownman <brownman556@gmail.com>
2015-11-22 13:29:57 +0200

    show git url before trying to clone

--- inside.sh
+++ inside.sh
@@ -5,22 +5,24 @@ user=${1:-user}
 subject=${2:-college}
 run_type=${3:-all}
 step_focus=${4:-}
-
+url1="https://gitlab.linnovate.net/brownman/dockerizing_projects.git"
+url2="git@gitlab.linnovate.net:brownman/dockerizing_projects.git"
 source $HOME/config.cfg
 
 
 commander_try test -d  /tmp/dockerizing_projects || { \
-( commander_try git clone https://gitlab.linnovate.net/brownman/dockerizing_projects.git /tmp/dockerizing_projects)
-(git clone https://gitlab.linnovate.net/brownman/dockerizing_projects.git /tmp/dockerizing_projects)  || (git clone git@gitlab.linnovate.net:brownman/dockerizing_projects.git /tmp/dockerizing_projects )
-
-
+commander_try git clone $url1 /tmp/dockerizing_projects
 }
-
+test $? -eq 0 && { \
 commander cd /tmp/dockerizing_projects
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+#git config --global user.email "you@example.com"
+#git config --global user.name "Your Name"
+commander_try cat .git/config 
 commander git add .
 commander git stash
 commander git pull
 commander chmod +x .ci.sh
 commander ./.ci.sh $user $subject $run_type $step_focus
+} || {
+  print error failed to clone
+}
