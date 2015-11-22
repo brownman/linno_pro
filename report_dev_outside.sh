ff50a7b83294c4a294daedff8c98a48218004343
brownman <brownman556@gmail.com>
2015-11-22 13:09:49 +0200

    Update report_dev_outside.sh

--- report_dev_outside.sh
+++ report_dev_outside.sh
@@ -15,4 +15,5 @@ trap_exit_outside(){
 cmd_hold_fingers="./outside.sh run ./report_dev_inside.sh"
 export -f trap_exit_outside
 #trap 'trap_exit1' EXIT
-( trap 'trap_exit_outside' EXIT; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
+
+( trap 'trap_exit_outside' ERR EXIT SIGHUP SIGINT SIGKILL SIGTERM SIGSTOP; commander_try "$cmd_hold_fingers  &> >(tee $file_report);"  )
