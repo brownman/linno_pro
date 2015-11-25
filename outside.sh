7bce88fbe7d5e820066a13e56aa1bf39fcbd71b8
brownman <brownman556@gmail.com>
2015-11-25 12:16:19 +0200

    Update outside.sh

--- outside.sh
+++ outside.sh
@@ -16,7 +16,7 @@ set_env(){
 
     volume_socket='-v /var/run/docker.sock:/var/run/docker.sock'
     volume_bin='-v /usr/bin/docker:/usr/bin/docker'
-    docker_cmd_it="docker run -it  --rm --name=$alias_ubuntu --privileged=true \
+    docker_cmd_it="docker run -it  --rm --default-network --name=$alias_ubuntu --privileged=true \
         $volume_ssh  \
         $volume_socket \
         $volume_bin \
@@ -25,7 +25,7 @@ set_env(){
         $container_id  \
         bash"
         
-    docker_cmd_i="docker run -i  --rm --name=$alias_ubuntu --privileged=true \
+    docker_cmd_i="docker run -i  --rm --default-network --name=$alias_ubuntu --privileged=true \
         $volume_ssh  \
         $volume_socket \
         $volume_bin \
@@ -82,8 +82,8 @@ trap_exit_outside_sh(){
     local res=$?
     print func
     cleanup
-    print warning try to run: 
-trace $docker_cmd_it
+   # print warning try to run: 
+#trace $docker_cmd_it
 return $res
 }
 
