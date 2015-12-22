dde9a3dba15132cd923f78316f7ff6f696eed143
brownman <brownman556@gmail.com>
2015-12-22 11:37:39 +0200

    docker install: disable prompt for apt-get install

--- docker_install.sh
+++ docker_install.sh
@@ -7,7 +7,7 @@ sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C
 sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-${distro} main \
     > /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get install docker-engine -y --force-yes
 sudo usermod -aG docker $LOGNAME
 }
 
